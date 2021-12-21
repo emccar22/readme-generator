e40ede6b0eab5c828bcc745323a6810f7d6989bd
emccar22 <e.mccarthy2285@gmail.com>
2021-12-20 19:21:18 -0500

    add badge rendering to generatemarkdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,36 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (!license) {
+    if (license == '') {
       return '';
+    } else if (license == 'ISC') {
+      return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+    } else if (license == 'MIT') {
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    } else if (license == 'Apache License 2.0') {
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    } else {
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
     }
-
-    return `
-
-    `;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == '') {
+    return '';
+  }
+
+  return `* [License](#license)`;
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (!license) {
+  if (license == '') {
     return '';
   }
 
-  return `
-  ## License
-
-
-  `;
+  return `## License`;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
 
 ## Description
 
@@ -40,18 +49,20 @@ ${data.description}
 
 * [Installation](#installation)
 * [Usage](#usage)
-* [License](#license)
+${renderLicenseLink(data.license)}
 * [Contribute](#contribute)
 * [Tests](#tests)
 * [Questions](#questions)
 
-## Installaation
+## Installation
 
 ${data.installation}
 
 ## Usage
 
 ${data.usage}
+
+${renderLicenseSection(data.license)}
 
 ## Contribute
 
@@ -63,7 +74,7 @@ ${data.tests}
 
 ## Questions
 
-If you have any questions please reach out to me on GitHub at [https://github.com/${data.github}](https://github.com/${data.github}) or Email me at [mailto:${data.email}](mailto:${data.email})
+If you have any questions please reach out to me on GitHub at [https://github.com/${data.github}](https://github.com/${data.github}) or Email me at [${data.email}](mailto:${data.email})
 `;
 }
 
